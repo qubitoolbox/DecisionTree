1ec456bd0cbdcab8aa633bf0faf4c3d5e904e544
qubitAI <osmanmorales@ymail.com>
2019-03-17 01:05:48 -0400

    Update BinaryCreateTest.java

--- BinaryCreateTest.java
+++ BinaryCreateTest.java
@@ -4,7 +4,19 @@ public class BinaryCreateTest
   public static void main(String[] args)
   {
 
-
+    BinarySearch bsearch = new BinarySearchTree();
+    
+    Integer root_ = 25;
+    Integer income_high = 5;
+    Integer income_medium = 3;
+    Integer income_low = 1;   
+    Integer student_yes = 0;
+    Integer student_no = 100;
+    Integer credit_fair = 0;
+    Integer credit_excellent = 90;
+    Integer class_label_n = 0;
+    Integer class_label_y = 100;
+    sctr.insert(root_)
 
 
   }
