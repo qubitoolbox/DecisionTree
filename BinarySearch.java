ebdf4c9a3090fc5134d3d4235c637bcbed264165
qubitAI <osmanmorales@ymail.com>
2019-03-17 00:41:49 -0400

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -3,7 +3,14 @@ import java.io.IOException;
 
 public class BinarySearchTree<AnyType extends Comparable<? super AnyType>>
 {
-
+  public static class BinaryNode
+  {
+    public Integer elem;
+    public BinaryNode leftNode;
+    public BinaryNode rightNode;
+  
+  
+  }
 
 
 
