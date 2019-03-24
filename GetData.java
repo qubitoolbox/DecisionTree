586643e8dd2a298e1e9cfc661ac6256e5eaae451
qubitAI <osmanmorales@ymail.com>
2019-03-23 22:34:06 -0400

    Adding Menu

---
+++ GetData.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class GetData {
+    
+    public static double getDouble(String s){
+        
+        return Double.parseDouble(getString(s));
+    }
+    
+    public static int getInt(String s){
+        
+        return Integer.parseInt(getString(s));
+    }
+    
+    public static String getString(String s){
+        
+        return JOptionPane.showInputDialog(s);
+    }
+    
+    
+}
