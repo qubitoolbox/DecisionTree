65efcff02543908ca5fe83837fbddfed3bf1cf7d
qubitAI <osmanmorales@ymail.com>
2019-04-02 23:00:56 -0400

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -104,7 +104,7 @@ public class BinarySearchTree<AnyType extends Comparable<? super AnyType>>
       String str = "";
       while(node!=null)
       {
-        str += node;// + " - 
+        str += node;
       }
       JTextArea text = new JTextArea(str, 20,30);
       JScrollPane pane = new JScrollPane(text);
