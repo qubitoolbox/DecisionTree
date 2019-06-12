fc243597f6c7bcf64154d6291ba8fc4b124efa19
qubitAI <osmanmorales@ymail.com>
2019-06-12 00:02:36 -0400

    Update GetData.java

--- GetData.java
+++ GetData.java
@@ -3,23 +3,21 @@ import javax.swing.JOptionPane;
 public class GetData 
 {
     
-    public static double getDouble(String s)
-    {
+  public static double getDouble(String s)
+  {
         
-        return Double.parseDouble(getString(s));
-    }
+    return Double.parseDouble(getString(s));
+  }
     
-    public static int getInt(String s)
-    {
+  public static int getInt(String s)
+  {
         
-        return Integer.parseInt(getString(s));
-    }
+    return Integer.parseInt(getString(s));
+  }
     
-    public static String getString(String s)
-    {
+  public static String getString(String s)
+  {
         
-        return JOptionPane.showInputDialog(s);
-    }
-    
-    
+    return JOptionPane.showInputDialog(s);
+  }  
 }
